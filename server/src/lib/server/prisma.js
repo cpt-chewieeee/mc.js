214b8e302987c9e33c952810253ac2cfd594e017
Hongyi Zhang <hzhan016@gmail.com>
2019-08-11 23:14:05 -0700

    push

--- server/src/lib/server/prisma.js
+++ server/src/lib/server/prisma.js
@@ -3,7 +3,7 @@ import { Prisma } from 'prisma-binding'
 const prisma = new Prisma({
   typeDefs: 'server/src/generated/prisma.graphql',
   endpoint: 'http://localhost:4466',
-  secret: 'thisismysupersecrettext'
+  secret: 'demo-secret'
 })
 
 export default prisma
